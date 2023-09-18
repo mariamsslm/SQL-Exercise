d32dbe1af323af46961c53ec23200b7b0ddbb434
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-09-18 21:16:38 +0300

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -1,19 +1,6 @@
 # Select name from students 
-# Select * from students where age>30
-# Select name from students where gender="F" and age>30
-# Select points from students where name="Alex"
-# INSERT INTO students values("7","Mariam","22","F","400")
-# UPDATE students SET points=points+100 where name="Basma"
-# UPDATE students SET points=points-100 where name="Alex"
-# INSERT INTO graduates(name,Age,Gender,Points,Graduation)
-# SELECT name,Age,Gender,Points,NULL
-# FROM students
-# WHERE name="Layal";
-
-# update graduates SET Graduation=("08/09/2018")
-
-# Delete from students where name="Layal"
-
-select name FROM employees where company=(select name from companies)
-select name FROM employees where Company =(select name from companies where date<2000)
-select name FROM employees where company=(select name from companies where role = "Graphic Designer")
+select name from students where points=(select MAX(points) from students)
+select AVG(points) from students
+select count(*) from students where points=500
+select name from students where name like "%s%"
+select * from students order by points DESC
