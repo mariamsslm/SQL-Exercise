76dcb84032df690dcb1ae8d653ed6589436ad56f
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-09-18 16:21:56 +0300

    1st commit

---
+++ answers.sh
@@ -0,0 +1,7 @@
+Select name from students 
+Select * from students where age>30
+Select name from students where gender="F" and age>30
+Select points from students where name="Alex"
+INSERT INTO students values("7","Mariam","22","F","400")
+UPDATE students SET points=points+100 where name="Basma"
+UPDATE students SET points=points-100 where name="Alex"
